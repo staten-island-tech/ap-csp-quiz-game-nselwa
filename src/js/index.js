2c2065ba6affda26bcdc0a078d1dd8e70c590ab9
nselwa <50632491+nselwa@users.noreply.github.com>
2020-10-21 19:39:39 -0400

    dasdas

--- src/js/index.js
+++ src/js/index.js
@@ -13,9 +13,41 @@ const init = function () {
     DOMSelectors.quizContainer.insertAdjacentHTML(
       "beforeend",
       `
-      
+      <h1 class= "question" id="${item.number}">Number ${item.number}: ${item.question}</h1>
+      <h2 class= "answer">
+        <label for="${item.choice}">${item.choice.a}</label>
+        <input type="radio" name="${item.number}" value="${item.choice.a}"  
+      </h2>
+      <h3 class= "answer">
+        <label for="${item.choice}">${item.choice.b}</label>
+        <input type="radio" name="${item.number}" value="${item.choice.b}"  
+      </h3>
+      <h4 class= "answer">
+        <label for="${item.choice}">${item.choice.c}</label>
+        <input type="radio" name="${item.number}" value="${item.choice.c}"  
+      </h4>
+      <h5 class= "answer">
+        <label for="${item.choice}">${item.choice.d}</label>
+        <input type="radio" name="${item.number}" value="${item.choice.d}"  
+      </h5>
       
       `
     )
   );
 };
+
+const quizResults = function () {
+  let score = 0;
+  quizQuestion.forEach((question) => {
+    const answerSelected = document.querySelector(
+      `input[name="${question.number}"]:checked`
+    ).value;
+    if (answerSelected === `${question.correctAnswer}`) {
+      score++;
+    }
+    DOMSelectors.scoreContainer.innerHTML = `<h1>You got ${score} questions correct!</h1>`;
+  });
+  DOMSelectors.submitButton.addEventListener("click", quizResults);
+};
+
+init();
