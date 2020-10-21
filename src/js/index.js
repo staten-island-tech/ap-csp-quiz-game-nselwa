79b385628986b6317520dc09725d991b9ed6c7b4
nselwa <50632491+nselwa@users.noreply.github.com>
2020-10-21 19:02:51 -0400

    asdasdf

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
+import { quizQuestion } from "./questions";
+
+console.log("connected");
+
 const DOMSelectors = {
   quizContainer: document.querySelector(".quiz-container"),
   scoreContainer: document.querySelector(".score-container"),
   submitButton: document.querySelector(".submit-btn"),
+};
+
+const init = function () {
+  quizQuestion.forEach((item) =>
+    DOMSelectors.quizContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      
+      
+      `
+    )
+  );
 };
--- src/js/questions.js
+++ src/js/questions.js
@@ -1,3 +1,5 @@
+export { quizQuestion };
+
 const quizQuestion = [
   {
     question: "",
